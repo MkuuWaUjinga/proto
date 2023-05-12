b8200b29694671898bd6c887d7f8329ca78fc3a4
Adrian <adrian.ziegler@tum.de>
2023-05-12 21:26:58 +0100

    feat: add skeleton

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,19 @@
+import 'server-only';
+import './globals.css';
+
+// do not cache this layout
+export const revalidate = 0;
+
+export default async function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+          {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
