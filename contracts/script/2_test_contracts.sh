397773ea2ac6bed88e985056c4f2bcde2a7948ea
Alexander Ludwig <alexanderludwig98@gmail.com>
2023-05-13 15:17:26 +0100

    fixed bugs in aave deposits

--- contracts/script/2_test_contracts.sh
+++ contracts/script/2_test_contracts.sh
@@ -2,8 +2,9 @@
 #tr '\r' '\n' < .env > .env.unix
 source .env
 
-deployedStrategist=0x52485A07D8717709FD2578C15Fdc8cb5e0B1e50f
-deployedDeposit=0xAE50CCbD0B6893858Ce5B956B8Ca7178D26A2aF1
+deployedDeposit=0xBA425ECBD5fEbA68D1C70a4533A71A990d61731B
+deployedStrategist=0x114FdA7148481E221A575E3Efa87ace590C2d430
+
 
 # 
 #cast rpc anvil_setStorageAt 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 $(cast keccak "[0x9b9E6E9c0C3578cAD98321eFb4e0651A507536CE,9]") 0x000000000000000000000000000000000000000000000000000000174876e800 --rpc-url http://localhost:8545 
@@ -13,7 +14,11 @@ deployedDeposit=0xAE50CCbD0B6893858Ce5B956B8Ca7178D26A2aF1
 # set allowance for the token to deposit it into the pool
 cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY $addressUSDC "approve(address,uint256)" $deployedDeposit 1000 
 
-#deposit funds locally in the pool
-cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY $deployedDeposit "deposit(address,uint256)" $addressUSDC 1000 
 
 
+#deposit and withdraw funds and check that funds are 0 eventually
+cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY $deployedDeposit "deposit(address,uint256)" $addressUSDC 1000
+cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY $deployedDeposit "withdraw(address,uint256)" $addressUSDC 1000 
+
+# check aToken balance for usdc
+cast call 0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c "balanceOf(address)(uint256)" 0xBA425ECBD5fEbA68D1C70a4533A71A990d61731B --rpc-url http://localhost:8545
--- contracts/src/Deposit.sol
+++ contracts/src/Deposit.sol
@@ -1,29 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
+//import "aave-v3-core/contracts/interfaces/IAToken.sol";
+
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "aave-v3-core/contracts/interfaces/IPool.sol";
 import "aave-v3-core/contracts/interfaces/IPoolAddressesProvider.sol";
+// import IAaveLendingPool
+import "aave-v3-core/contracts/interfaces/IPool.sol";
+// import console2
+import {console2} from "lib/forge-std/src/console2.sol";
+
 
 contract DepositModule {
 
     // Aave V3 Lending Pool Addresses Provider address
-    address private constant LENDING_POOL_ADDRESSES_PROVIDER = 0x0000000000000000000000000000000000000000; // Replace with the correct address
+    address private constant LENDING_POOL_ADDRESSES_PROVIDER = address(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e); // Replace with the correct address
 
     // Mapping from token address to mapping of account balances
     mapping(address => uint256) private _balances;
+
+    // all for usdc
+    IERC20 public usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    address public aToken = 0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c;
+    IPool public pool = IPool(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
+
 
     function deposit(address token, uint256 amount) external {
         // Transfer the tokens to this contract
         IERC20(token).transferFrom(msg.sender, address(this), amount);
 
-        // Approve Aave Lending Pool to use the tokens
-        IERC20(token).approve(_getLendingPool(), amount);
+        // // Approve Aave Lending Pool to use the tokens
+        IERC20(token).approve(address(pool), amount);
 
-        // Deposit the tokens into the Aave Lending Pool
-        IPool(_getLendingPool()).deposit(token, amount, address(this), 0);
+        //IPoolAddressesProvider provider = IPoolAddressesProvider(address(0x24a42fD28C976A61Df5D00D0599C34c4f90748c8)); 
+        //IPool lendingPool = IPool(provider.getPool());
+        //console2.log(address(lendingPool));
+        console2.log(address(pool));
+        // // Deposit the tokens into the Aave Lending Pool
+        pool.deposit(token, amount, address(this), 1000);
 
-        // Update the balance
+        // // Update the balance
         _balances[msg.sender] += amount;
     }
 
@@ -32,16 +49,21 @@ contract DepositModule {
 
         // Update the balance
         _balances[msg.sender] -= amount;
+        pool.withdraw(token, amount, address(this));
 
         // Withdraw the tokens from the Aave Lending Pool
-        IPool(_getLendingPool()).withdraw(token, amount, msg.sender);
+        //IPool(_getLendingPool()).withdraw(token, amount, msg.sender);
     }
+
 
     function balanceOf(address token, address account) external view returns (uint256) {
         return _balances[account];
     }
 
-    function _getLendingPool() private view returns (address) {
-        return IPoolAddressesProvider(LENDING_POOL_ADDRESSES_PROVIDER).getPool();
+    // function that returns the total amount of funds deposited in aave
+    function getTotalDeposited(address token) external view returns (uint256) {
+        return IERC20(aToken).balanceOf(address(this));
     }
+
+
 }
